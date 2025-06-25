dade674d026c090130c4580738dbc1040c5c308d
Scorpius-S <unknowncorp@live.co.uk>
2025-06-25 21:43:20 +0100

    Commit E

--- functions/get_files_infor.py
+++ functions/get_files_infor.py
@@ -2,6 +2,12 @@ import os #Importing os module to interact with the operating system
 
 def get_files_info(working_directory, directory=None):
     if directory is None:
-            directory = working_directory
-            if not os.path.exists(directory):
-                  raise f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        directory = working_directory
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_directory = os.path.abspath(directory)
+
+    if not abs_directory.startswith(abs_working_directory):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(directory):
+        return f'Error: "{directory}" is not a directory'
+            
