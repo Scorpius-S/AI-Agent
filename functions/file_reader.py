9f09571e5934a0f651fb63ce8012a6e4395dcedd
Scorpius-S <unknowncorp@live.co.uk>
2025-07-01 22:31:53 +0100

    Commit J: Finished Get Fule Content Functionality

--- functions/file_reader.py
+++ functions/file_reader.py
@@ -2,14 +2,16 @@ import os
 
 def get_file_content(working_directory, file_path):
     abs_working_directory = os.path.abspath(working_directory)
-    abs_file_path = os.path.abspath(file_path)
-    if not abs_file_path.startswith(abs_working_directory):
+    combined_path = os.path.join(abs_working_directory, file_path)
+    if not combined_path.startswith(abs_working_directory):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(abs_file_path):
+    if not os.path.isfile(combined_path):
         return f'Error: File not found or is not a regular file: {file_path}'
+    file_content = read_with_truncation(combined_path)
+    return file_content
     
-def read_with_truncation(file_path, max_chars=10000):
-    abs_path = os.path.abspath(file_path)
+def read_with_truncation(combined_path, max_chars=10000):
+    abs_path = os.path.abspath(combined_path)
     content = ""
     try:
         with open(abs_path, 'r', encoding='utf-8', errors='replace') as f:
@@ -18,8 +20,8 @@ def read_with_truncation(file_path, max_chars=10000):
             if overflow:
                 content += (
                     f"\n\n"
-                    f"[...File \"{file_path}\" truncated at {max_chars} characters]"
+                    f"[...File \"{combined_path}\" truncated at {max_chars} characters]"
                 )
         return content
     except IOError as e:
-        return f"Error: Failed to read file \"{file_path}\": {e}"
+        return f"Error: Failed to read file \"{combined_path}\" due to {str(e)}"
--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
-from functions.get_files_info import get_files_info
+from functions.file_reader import get_file_content, read_with_truncation
 
-print(get_files_info(".", "."))
-print(get_files_info("calculator", "pkg"))
-print(get_files_info(".", "/bin"))
-print(get_files_info(".", "../"))
+print(get_file_content("calculator", "lorem.txt"))
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat"))
 
 
 
