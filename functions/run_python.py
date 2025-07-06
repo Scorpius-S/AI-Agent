c9c75413d93bbffd06a7753a9938bc22c89a32aa
Scorpius-S <unknowncorp@live.co.uk>
2025-07-06 14:29:23 +0100

    Commit L: Began writing a function to run Python file with security checks

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,3 +1,7 @@
 import os
 
 def run_python_file(working_directory, file_path):
+    working_directory = os.path.abspath(working_directory)
+    file_path = os.path.abspath(file_path)
+    if not file_path.startswith(working_directory):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
